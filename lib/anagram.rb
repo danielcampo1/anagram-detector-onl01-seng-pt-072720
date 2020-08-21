948c8b3516187823ff609feb6ab8b5363c3efb97
daniel campo <campo_daniel94@yahoo.com>
2020-08-21 17:51:06 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+require'pry'
+class Anagram
+  
+  attr_accessor :match
+  
+ def  initialize(match)
+  @word = match
+end
+  
+  def match(words_array)
+
+    word_sorted = @word.split("").sort
+    matches = []
+    test = words_array.collect { |w| w.split("") }
+    test = test.collect { |l| l.sort }
+
+      test.collect do |t|
+# binding.pry
+      if t == word_sorted
+      matches << words_array[test.find_index(t)]
+      end
+    end
+    matches
+  end
+  
+end
