3f6c64e3f6828eefa2cb24c959ffe948361cabb9
daniel campo <campo_daniel94@yahoo.com>
2020-08-21 18:30:00 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -17,15 +17,7 @@ end
       if word_sorted = word.split("").sort 
         matches << word
     
-    test = words_array.collect { |w| w.split("") }
-    test = test.collect { |l| l.sort }
-
-      test.collect do |t|
-# binding.pry
-      if t == word_sorted
-      matches << words_array[test.find_index(t)]
-      end
-    end
+  end
     matches
   end
   
