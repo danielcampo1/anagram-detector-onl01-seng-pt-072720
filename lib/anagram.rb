9f6da189b1fca2d66109020a7b991b270c1baf97
daniel campo <campo_daniel94@yahoo.com>
2020-08-21 17:57:11 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -17,7 +17,8 @@ end
 
       test.collect do |t|
 # binding.pry
-      if t == word_sorted
+      if t
+        binding.pry == word_sorted
       matches << words_array[test.find_index(t)]
       end
     end
